a8edc477900c5c52af7d6fe8139b4a33957d6d0f
ankita narvekar <23829237@student.uwa.edu.au>
2024-09-01 01:01:50 +0800

    Adding the html, css and js files for app header and grouping page

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,3 +4,7 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/grouping-page')
+def grouping():
+    return render_template('grouping-page.html') 
