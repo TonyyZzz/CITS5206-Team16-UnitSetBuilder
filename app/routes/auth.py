023f4a068327efdef2acb5a4df81f8c4351694a1
Joel Qian <rfc@live.com>
2024-09-05 14:42:56 +0800

    add registration functionality

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,9 +2,16 @@ from flask import render_template, request, jsonify
 from flask_login import login_user, login_required, logout_user
 from flask_bcrypt import Bcrypt
 from app.models import User
-from app import login_manager, bcrypt
+from app import login_manager, bcrypt, db
 from . import auth
 
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.query.get(user_id)
+    except:
+        return None
+    
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -21,9 +28,22 @@ def login():
     else:
         return render_template('login.html')
     
-@login_manager.user_loader
-def load_user(user_id):
-    try:
-        return User.query.get(user_id)
-    except:
-        return None
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Handle register form submission
+        data = request.json
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return jsonify({'status': 'error', 'message': 'Username already exists'})
+        else:
+            new_user = User(username=username, email=email, password_hash=bcrypt.generate_password_hash(password).decode('utf-8'))
+            # Add new user to the database
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify({'status': 'success'})
+    else:
+        return render_template('register.html')
